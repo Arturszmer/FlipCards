a637cf49fd2c4470e89d8fb0647be902ee3a075e
arturszmer <arturszmer@gmail.com>
2022-10-23 09:31:42 +0200

    first commit

---
+++ src/main/java/com/example/flipcardsapp/FlipCardsAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.flipcardsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlipCardsAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlipCardsAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/flipcardsapp/FlipCardsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flipcardsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlipCardsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
